4243a66a4cc355ef9fdc922f902e015e2b8ac45c
Blanca Rivero <briveroanglada@gmail.com>
2021-11-16 16:48:06 +0100

    create method

--- app/controllers/outfits_controller.rb
+++ app/controllers/outfits_controller.rb
@@ -4,4 +4,17 @@ class OutfitsController < ApplicationController
     @outfit = Outfit.new
   end
 
+  def create
+    @outfit = Outfit.new(outfit_params)
+    @outfit.save
+
+    redirect_to outfit_path(@outfit)
+  end
+
+  private
+
+  def outfit_params
+    params.require(:outfit).permit(:name, :size, :price, :max_loan_period)
+  end
+
 end
