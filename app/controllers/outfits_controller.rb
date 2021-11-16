c8a989f28f2306ed01d5f8f9356478735e9c6d0f
Emily Underhill <emilyru93@gmail.com>
2021-11-16 16:08:52 +0000

    edit & update methods created in outfit controller

--- app/controllers/outfits_controller.rb
+++ app/controllers/outfits_controller.rb
@@ -1,4 +1,5 @@
 class OutfitsController < ApplicationController
+  before_action :set_outfit, only: %i[show edit]
 
   def new
     @outfit = Outfit.new
@@ -11,18 +12,28 @@ class OutfitsController < ApplicationController
     redirect_to outfit_path(@outfit)
   end
 
-  def show
-    @outfit = Outfit.find(params[:id])
-  end
+  def show; end
 
   def index
     @outfits = Outfit.all
+  end
+
+  def edit; end
+
+  def update
+    @oufit.update(outfit_params)
+
+    redirect_to outfit_path(@outfit)
   end
 
   private
 
   def outfit_params
     params.require(:outfit).permit(:name, :size, :price, :max_loan_period)
+  end
+
+  def set_outfit
+    @outfit = Outfit.find(params[:id])
   end
 
 end
