c85b3a269c45ebb5cf228b079258e6cc47691075
manrika malka <manrika.m@gmail.com>
2021-11-17 12:04:58 +0000

    authentication changes & new booking

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,10 +1,32 @@
 class BookingsController < ApplicationController
+  before_action :set_outfit, only: [:new, :create, :index]
+
   def new
     @booking = Booking.new
   end
 
+  def create
+    @booking = Booking.new(booking_params)
+    @booking.outfit = @outfit
+    @booking.user = @outfit.user
+    if @booking.save
+      redirect_to outfit_path(@outfit)
+    else
+      render :new
+    end
+  end
+
   def index
-    @outfit = Outfit.find(params[:outfit_id])
     @bookings = Booking.where(outfit_id: params[:outfit_id])
+  end
+
+  private
+
+  def booking_params
+    params.require(:booking).permit(:date_from, :date_to)
+  end
+
+  def set_outfit
+    @outfit = Outfit.find(params[:outfit_id])
   end
 end
