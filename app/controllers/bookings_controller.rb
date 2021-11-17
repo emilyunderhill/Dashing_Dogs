700f4b4e2579bf893603ff987857eff4c6780fc3
Blanca Rivero <briveroanglada@gmail.com>
2021-11-17 12:47:35 +0100

    controller update booking

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -7,4 +7,21 @@ class BookingsController < ApplicationController
     @outfit = Outfit.find(params[:outfit_id])
     @bookings = Booking.where(outfit_id: params[:outfit_id])
   end
+
+  def edit
+    @booking = Booking.find(params[:id])
+  end
+
+  def update
+    @booking = Booking.find(params[:id])
+    @booking.update(booking_params)
+
+    redirect_to booking_path(@booking)
+  end
+
+  private
+
+  def booking_params
+    params.require(:booking).permit(:date_from, :date_to)
+  end
 end
